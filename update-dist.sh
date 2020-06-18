a27831dbaf0ad92cdf73ae0853e85fe00834a979
Charlton Lim <cadmusthefounder@gmail.com>
2020-06-18 15:56:58 +0800

    add mac os support

--- update-dist.sh
+++ update-dist.sh
@@ -7,7 +7,13 @@ pushd flask_swagger_ui/dist > /dev/null
 
 OUTPUT_FILE=${RELEASE}.tar.gz
 curl -s https://api.github.com/repos/swagger-api/swagger-ui/releases/latest | grep tarball_url | cut -d '"' -f 4 | wget -q -O "${OUTPUT_FILE}" -i -
-tar --strip-components=2 -xvf ${OUTPUT_FILE} '*/dist'
+
+if [ "$(uname)" == "Darwin" ]; then
+    tar --strip-components=2 -xvf ${OUTPUT_FILE} '*/dist'
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    tar --wildcards --strip-components=2 -xvf ${OUTPUT_FILE} '*/dist'
+fi
+
 echo "${RELEASE}" > VERSION
 rm index.html
 rm ${OUTPUT_FILE}
