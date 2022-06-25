eb74f8aadb760856ff9b36e6b0a0280e553a3ace
Francesco Fornari <francesco.fornari.96@gmail.com>
2022-06-25 18:53:39 +0200

    ADD: possibility to configure with custom icon

--- example.py
+++ example.py
@@ -1,13 +1,20 @@
-from flask import Flask
+from flask import Flask, url_for
 from flask_swagger_ui import get_swaggerui_blueprint
 
 app = Flask(__name__)
+app.config['SERVER_NAME'] = "127.0.0.1:5000"
 
 
 SWAGGER_URL = "/api/docs"  # URL for exposing Swagger UI (without trailing '/')
 API_URL = (
     "http://petstore.swagger.io/v2/swagger.json"
 )  # Our API url (can of course be a local resource)
+
+with app.app_context():
+    icons = [{
+        "href": url_for("static", filename="favicon.ico"),
+        "sizes": "32x32" # optional key
+    }]
 
 # Call factory function to create our blueprint
 swaggerui_blueprint = get_swaggerui_blueprint(
@@ -22,10 +29,11 @@ swaggerui_blueprint = get_swaggerui_blueprint(
     #    'scopeSeparator': " ",
     #    'additionalQueryStringParams': {'test': "hello"}
     # }
+    # icons=icons
 )
 
 app.register_blueprint(swaggerui_blueprint)
 
-app.run()
+app.run(host="127.0.0.1")
 
 # Now point your browser to localhost:5000/api/docs/
