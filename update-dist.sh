1c07dc248d5c38e01c03f2906b36dd006436e21c
Alex Ley <alex@scrub-ai.com>
2026-03-22 22:21:34 +0000

    fix(update-dist.sh): auto update README.md and setup.py and auto tag the commit

--- update-dist.sh
+++ update-dist.sh
@@ -19,5 +19,12 @@ rm ${OUTPUT_FILE}
 popd > /dev/null
 
 RELEASE_NUMBER=$(echo "${RELEASE}" | sed 's/^v//g')
+# use sed to update the version in setup.py, which is in the format version="x.y.z"
+sed -i '' -E "s/version=\"[0-9]+\.[0-9]+\.[0-9]+\"/version=\"${RELEASE_NUMBER}\"/g" setup.py
+# use sed to update the version in README.md, which is in the format 'Included Swagger UI version: x.y.z'
+sed -i '' -E "s/Included Swagger UI version: [0-9]+\.[0-9]+\.[0-9]+/Included Swagger UI version: ${RELEASE_NUMBER}/g" README.md
+
+git add README.md setup.py
 git add flask_swagger_ui/dist
 git commit -m "Update to version ${RELEASE_NUMBER}" -m "see $RELEASES_URL/tag/${RELEASE}"
+git tag $RELEASE
