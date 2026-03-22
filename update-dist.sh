820286e6fae809054ab300f48a516bc0fdd26d98
Alex Ley <alex@scrub-ai.com>
2026-03-22 21:34:12 +0000

    fix(update-dist.sh): remove wget dependency and just use curl twice

--- update-dist.sh
+++ update-dist.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-RELEASE=$(curl -s https://api.github.com/repos/swagger-api/swagger-ui/releases/latest | grep tag_name | cut -d '"' -f 4)
+LATEST_RELEASE=$(curl -s https://api.github.com/repos/swagger-api/swagger-ui/releases/latest)
+RELEASE=$(echo "$LATEST_RELEASE" | grep tag_name | cut -d '"' -f 4)
 echo "Downloading release ${RELEASE}"
 
 pushd flask_swagger_ui/dist > /dev/null
 
 OUTPUT_FILE=${RELEASE}.tar.gz
-curl -s https://api.github.com/repos/swagger-api/swagger-ui/releases/latest | grep tarball_url | cut -d '"' -f 4 | wget -q -O "${OUTPUT_FILE}" -i -
+TARBALL_URL=$(echo "$LATEST_RELEASE" | grep tarball_url | cut -d '"' -f 4)
+curl -sL -o "${OUTPUT_FILE}" "${TARBALL_URL}"
 # Check if GNU or BSD tar
 WILDCARD_FLAG=$(tar --version | grep -q GNU && echo "--wildcards" || echo "")
 tar $WILDCARD_FLAG --strip-components=2 -xvf ${OUTPUT_FILE} '*/dist'
